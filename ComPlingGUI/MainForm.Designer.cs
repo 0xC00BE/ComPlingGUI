2008a106db40adaa791b9b80c15893633bcbbea1
Cuboctahedron <9017544+0xC00BE@users.noreply.github.com>
2022-08-23 11:04:04 +0200

    Add files via upload

---
+++ ComPlingGUI/MainForm.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace ComPlingGUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnControl = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbNIC = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rtbNetworks = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnControl
+            // 
+            this.btnControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnControl.Location = new System.Drawing.Point(144, 255);
+            this.btnControl.Name = "btnControl";
+            this.btnControl.Size = new System.Drawing.Size(212, 59);
+            this.btnControl.TabIndex = 2;
+            this.btnControl.Text = "Start";
+            this.btnControl.UseVisualStyleBackColor = true;
+            this.btnControl.Click += new System.EventHandler(this.btnControl_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Networks - one per line in  CIDR notation:";
+            // 
+            // cbNIC
+            // 
+            this.cbNIC.FormattingEnabled = true;
+            this.cbNIC.Location = new System.Drawing.Point(116, 12);
+            this.cbNIC.Name = "cbNIC";
+            this.cbNIC.Size = new System.Drawing.Size(375, 21);
+            this.cbNIC.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Network interface: ";
+            // 
+            // rtbNetworks
+            // 
+            this.rtbNetworks.Location = new System.Drawing.Point(16, 63);
+            this.rtbNetworks.Name = "rtbNetworks";
+            this.rtbNetworks.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.rtbNetworks.Size = new System.Drawing.Size(475, 186);
+            this.rtbNetworks.TabIndex = 6;
+            this.rtbNetworks.Text = resources.GetString("rtbNetworks.Text");
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 345);
+            this.Controls.Add(this.rtbNetworks);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbNIC);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnControl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "ComPling";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnControl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbNIC;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox rtbNetworks;
+    }
+}
+
---
+++ ComPlingGUI/MainForm.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComPlingGUI
+{
+    public partial class MainForm : Form
+    {
+        private string tsharkPath = "";
+        private string programDir = Environment.CurrentDirectory.ToString();
+        private bool running = false;
+        private Process process = new Process();
+        private List<int> streams = new List<int>();
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            string pfx86Path = @"C:\Program Files (x86)\Wireshark\tshark.exe";
+            string pfPath = @"C:\Program Files\Wireshark\tshark.exe";
+            if (File.Exists(pfPath))
+            {
+                tsharkPath = pfPath;
+                
+            }
+            if (File.Exists(pfx86Path))
+            {
+                tsharkPath = pfx86Path;
+            }
+            if (tsharkPath == "")
+            {
+                MessageBox.Show("Wireshark installation not found.", "File not found!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+
+            var tcpDumpProc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = tsharkPath,
+                    Arguments = "-D",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+            tcpDumpProc.Start();
+            while (!tcpDumpProc.StandardOutput.EndOfStream)
+            {
+                string line = tcpDumpProc.StandardOutput.ReadLine();
+                cbNIC.Items.Add(line);
+            }
+
+            
+            string networksFile = programDir + @"\networks.txt";
+            if (File.Exists(networksFile))
+            {
+                rtbNetworks.Text = File.ReadAllText(networksFile);
+            }
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            string networksFile = programDir + @"\networks.txt";
+            File.WriteAllText(networksFile, rtbNetworks.Text);
+            try
+            {
+                if (!process.HasExited)
+                {
+                    process.Kill();
+                }
+            }
+            catch { }
+        }
+
+        private void StartProcess(string nic, string filter)
+        {
+            ProcessStartInfo StartInfo = new ProcessStartInfo
+            {
+                FileName = tsharkPath,
+                Arguments = "-i " + nic + " -f \"(" + filter + ") and (dst port 80 or dst port 443)\" -T fields -e tcp.len -l",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true,
+            };
+            process.StartInfo = StartInfo;
+            process.OutputDataReceived += (sender, args) => ProcessData(args.Data);
+            process.ErrorDataReceived += (sender, args) => ProcessData(args.Data);
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+
+        }
+        private void ProcessData(string data)
+        {
+            if (Int32.TryParse(data, out int val))
+            {
+
+                if (val > 0)
+                {
+                    Console.Beep(250, 100);
+                }
+            }
+            
+        }
+
+        private void btnControl_Click(object sender, EventArgs e)
+        {
+            if (running)
+            {
+                process.Kill();
+                btnControl.Text = "Start";
+                btnControl.BackColor = SystemColors.Control;
+                running = false;
+            } else
+            {
+                btnControl.Text = "Stop";
+                btnControl.BackColor = Color.FromName("Green");
+                string nic = cbNIC.Items[cbNIC.SelectedIndex].ToString().Split('.')[0];
+                string filter = "dst net ";
+
+                foreach (string line in rtbNetworks.Lines)
+                {
+                    filter += line + " or dst net ";
+                }
+
+                filter = filter.Remove(filter.Length - 12);
+                StartProcess(nic, filter);    
+                running = true;
+            }
+        }
+    }
+}
